da75a7892c2de150d177a30fee8c1303d3541f7d
Anna Gargiulo <a.gargiulo45@studenti.unisa.it>
2023-02-15 11:33:35 +0100

    fixed error in print

--- DatasetPrep/VariablePreSelection.py
+++ DatasetPrep/VariablePreSelection.py
@@ -3,8 +3,8 @@ from sklearn.feature_selection import VarianceThreshold
 
 def feature_pre_selection(data, data_np, threshold=0):
     vt = VarianceThreshold()
-    data_np = vt.fit_transform(data_np)
-    print("[INFO] Removed ", data.shape[1] - data_np.shape[1], " features with variance = ", threshold, " along samples")
+    data_removed = vt.fit_transform(data_np)
+    print("[INFO] Removed ", data_np.shape[1] - data_removed.shape[1], " features with variance = ", threshold, " along samples")
     selected_features = list()
     features = list(data.columns)
     array = vt.get_support(True)
